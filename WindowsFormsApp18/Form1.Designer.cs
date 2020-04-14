9f0192f6833e5212c73ea231cbd4e8a06b272f8b
shota_zaali90 <shota_zaali90@hotmail.com>
2020-04-14 23:57:45 +0400

    Change

---
+++ WindowsFormsApp18/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace WindowsFormsApp18
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gagzavna = new System.Windows.Forms.Button();
+            this.nomeri = new System.Windows.Forms.TextBox();
+            this.textisveli = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gagzavna
+            // 
+            this.gagzavna.Location = new System.Drawing.Point(470, 379);
+            this.gagzavna.Name = "gagzavna";
+            this.gagzavna.Size = new System.Drawing.Size(193, 39);
+            this.gagzavna.TabIndex = 0;
+            this.gagzavna.Text = "გაგზავნა";
+            this.gagzavna.UseVisualStyleBackColor = true;
+            this.gagzavna.Click += new System.EventHandler(this.gagzavna_Click);
+            // 
+            // nomeri
+            // 
+            this.nomeri.Location = new System.Drawing.Point(25, 54);
+            this.nomeri.Multiline = true;
+            this.nomeri.Name = "nomeri";
+            this.nomeri.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.nomeri.Size = new System.Drawing.Size(251, 319);
+            this.nomeri.TabIndex = 1;
+            // 
+            // textisveli
+            // 
+            this.textisveli.Location = new System.Drawing.Point(314, 54);
+            this.textisveli.Multiline = true;
+            this.textisveli.Name = "textisveli";
+            this.textisveli.Size = new System.Drawing.Size(349, 319);
+            this.textisveli.TabIndex = 2;
+            this.textisveli.TextChanged += new System.EventHandler(this.textisveli_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(242, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ტელეფონის ნომრები Enter-ის გამოყენებით";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(688, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(311, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "შეტყობინების ტექსტი";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(688, 422);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textisveli);
+            this.Controls.Add(this.nomeri);
+            this.Controls.Add(this.gagzavna);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "SMS Sender";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button gagzavna;
+        private System.Windows.Forms.TextBox nomeri;
+        private System.Windows.Forms.TextBox textisveli;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApp18/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+//by Shota Abzhandadze
+namespace WindowsFormsApp18
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void gagzavna_Click(object sender, EventArgs e)
+        {
+
+            SubmitData();
+        }
+        private void SubmitData()
+        {
+            try
+            {
+                string a = nomeri.Text;
+                string[] lst = a.Split(new Char[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+                string b = textisveli.Text;
+
+                WebRequest request = WebRequest.Create("http://YOURADDRESS/mt/oneway?username=YOURUSERNAME&password=YOURPASSWORD&client_id=YOURCLIENTID&service_id=YOURSERVICEID&to=" + a + "&text=" + b);
+                request.Method = "POST";
+                request.ContentType = "application/x-www-form-unlencoded";
+
+                Stream stream = request.GetRequestStream();
+                stream.Close();
+
+                WebResponse response = request.GetResponse();
+                stream = response.GetResponseStream();
+
+                StreamReader sr = new StreamReader(stream);
+                MessageBox.Show(sr.ReadToEnd());
+
+                sr.Close();
+                stream.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error : " + ex.Message);
+            }
+        }
+
+        private void textisveli_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("by Shota Abzhandadze");
+        }
+    }
+}
